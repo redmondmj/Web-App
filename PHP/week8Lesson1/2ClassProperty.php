a2e5f7ea008d03aa53440dcf36d0dbeef6b3ea02
JohnFMacDonald <w0279013@nscc.ca>
2016-02-24 09:45:17 -0400

    Class break time

---
+++ PHP/week8Lesson1/2ClassProperty.php
@@ -0,0 +1,15 @@
+<?php
+
+class MyClass
+{
+
+}
+
+$obj = new MyClass;
+
+// Output the property
+//(->) is an OOP construct that accesses the contained properties and methods of a given object
+
+?>
+
+
---
+++ PHP/week8Lesson1/6ClassVisibility.php
@@ -0,0 +1,50 @@
+<?php
+
+//Visibility
+//Protected - means it can only be accessed within the class itself or in descendant classes
+//Private - only from within the class that defines it
+
+class MyClass {
+    public $prop1 = "I'm a class property!";
+
+    public function __construct() {
+        echo 'The class "', __CLASS__, '" was initiated!<br />';
+    }
+
+    public function __destruct() {
+        echo 'The class "', __CLASS__, '" was destroyed.<br />';
+    }
+
+    public function __toString() {
+        echo "Using the toString method: ";
+        return $this->getProperty();
+    }
+
+    public function setProperty($newval) {
+        $this->prop1 = $newval;
+    }
+
+    public function getProperty() {
+        return $this->prop1 . "<br />";
+    }
+}
+
+class MyOtherClass extends MyClass {
+    public function __construct() {
+        parent::__construct();
+        echo "A new constructor in " . __CLASS__ . ".<br />";
+    }
+
+    public function newMethod() {
+        echo "From a new method in " . __CLASS__ . ".<br />";
+    }
+
+}
+
+// Create a new object
+$newobj = new MyOtherClass;
+
+// Call the protected method from within a public method
+echo $newobj->getProperty();
+
+?>
