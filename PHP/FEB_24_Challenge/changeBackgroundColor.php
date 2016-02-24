fe0295e944703b63ff4efe5623b29022e9bf4b15
JohnFMacDonald <w0279013@nscc.ca>
2016-02-24 01:01:12 -0400

    Completed

--- PHP/FEB_24_Challenge/changeBackgroundColor.php
+++ PHP/FEB_24_Challenge/changeBackgroundColor.php
@@ -1,13 +1,27 @@
 <?php
-session_start();
 
-if(isset($_POST['colourset'])) {
-$colour = $_POST['colourset'];
-$_SESSION['colourset'] = $colour;
+$cookie_name = "colourhex";
+$count_name = "count";
+$message = "Okay, now get back to work.";
+
+if (isset($_POST["submit"])){
+    $cookie_value = $_POST['colourset'];
+    if (isset($_COOKIE[$count_name])){
+        $cookie_count = $_COOKIE[$count_name]-1;
+    }else{
+        $cookie_count = 3;
+    }
+}
+else if(isset($_COOKIE[$cookie_name])) {
+    $cookie_value = $_COOKIE[$cookie_name];
+    $cookie_count = 3;
+}else{
+    $cookie_value="FFFFFF";
+    $cookie_count = 3;
 }
 
-$colour_session = $_SESSION['colourset'];
-echo "bgcolour = $colour_session";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+setcookie($count_name, $cookie_count, time() + (86400 * 30), "/"); // 86400 = 1 day
 
 ?>
 
@@ -15,13 +29,27 @@ echo "bgcolour = $colour_session";
 <html>
     <head>
         <title>Background Challenge</title>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="styles.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <style type="text/css">
+            body { background: <?php echo "#".$cookie_value ?> !important; } /* Adding !important forces the browser to overwrite the default style applied by Bootstrap */
+        </style>
     </head>
 
-    <body bgcolor='#<?php echo $colour_session; ?>'>
-        <form id="form1" name="form1" method="post" action="changeBackgroundColor.php">
-            <input name="colourset" placeholder="Color Hex"/>
-            <input type="submit" value="Change Color">
+    <body>
+        <form class="well mywell col-sm-8 col-sm-offset-2" id="form1" name="form1" method="post" action="changeBackgroundColor.php">
+            <div class="h4">
+            <?php if($cookie_count!=0){echo $cookie_count . " guesses left.";}else{echo $message;} ?><br/>
+            </div>
+            <div class="col-xs-8">
+            <input class="form-control" name="colourset" placeholder="Color Hex"/>
+            </div>
+            <input <?php if ($cookie_count == 0){ ?> disabled <?php } ?> type="submit" class="btn btn-danger col-xs-4" name="submit" value="Change Color">
+
         </form>
     </body>
 <html>
