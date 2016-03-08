fb5c62debe476f208da04e83b739b2bdcda0f213
JohnFMacDonald <w0279013@nscc.ca>
2016-03-08 15:12:31 -0400

    Working files for challenge

---
+++ RIA/helloWorldAngular/App.js
@@ -0,0 +1,46 @@
+/*jshint browser:true */
+/*jshint devel:true */
+/*globals angular */
+
+(function() {
+    "use strict";
+
+    // declare the application module
+    var app = angular.module("HelloWorld",[]);
+    app.controller("HelloController", function(){
+        // add a data property
+        this.course = data.course;
+        this.enteredName="";
+        this.hide=false;
+        this.goodBye=function(){
+            return "GOOD-BYE!";
+        }
+    });
+
+    // hard coded JSON data (for now)
+    var data = {
+        course: {
+            name: "Rich Internet Applications",
+            instructor: "Sean Morrow",
+            students: [
+                {
+                    id: "w0012345",
+                    name: "Geoff White"
+                },
+                {
+                    id: "w114578",
+                    name: "John MacDonald"
+                },
+                {
+                    id: "w012457",
+                    name: "Dan Rutledge"
+                },
+                {
+                    id: "w001178",
+                    name: "Kyle Lee"
+                }
+            ]
+        }
+    }
+
+}());
